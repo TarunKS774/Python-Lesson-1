164c5c0de6dbcc5f34d8e0bd14a0159fe2f07177
TarunKS774 <tarunkuduvaa01@gmail.com>
2026-06-02 12:33:00 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,11 @@
+# activity 2
+# making variable name and including it in a print statement
+name="Tarun"
+print("Hello",name)
+# printing statement hello python and new line
+print("Hello python \n")
+#printing number
+print("Hello 5")
+#printing and ending the code
+print("Bye!",end="*")
+
