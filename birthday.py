83c3c8840f1f9f077a213e5d0bdbd222506a7a91
TarunKS774 <tarunkuduvaa01@gmail.com>
2026-06-04 13:08:32 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,12 @@
+birthdayTarun = "December 8 2010"
+birthdayKelvin = "August 2 2010"
+birthdayRanveer = "September 18 2010"
+birthdayShaam = "January 4 2010"
+birthdayAmir = "March 3 2010"
+
+print("Tarun's birthday is on",birthdayTarun)
+print("Kelvin's birthday is on",birthdayKelvin)
+print("Ranveer's birthday is on",birthdayRanveer)
+print("Shaam's birthday is on",birthdayShaam)
+print("Amir's birthday is on",birthdayAmir)
+
